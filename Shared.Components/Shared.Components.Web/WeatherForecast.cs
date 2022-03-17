363123ccc6049888ab500162635f7d7a827e165f
Peterson <petersonzeferino@hotmail.com>
2022-03-17 17:46:31 -0300

    Test trigger

--- Shared.Components/Shared.Components.Web/WeatherForecast.cs
+++ Shared.Components/Shared.Components.Web/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace Shared.Components.Web
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string Teste { get; set; }
     }
 }
